886e9df075316f8ac53a6a59d014d17b525941ed
me-gonzalez674 <me.gonzalez674@gmail.com>
2026-04-16 10:32:12 -0500

    Update graphing.py

--- soot_tool/graphing.py
+++ soot_tool/graphing.py
@@ -87,7 +87,7 @@ def make_plot(
     show_smoothed: bool = True,
     title: str = f"NASA SOOT Visualization",
 ) -> matplotlib.figure.Figure:
-    fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
+    fig = matplotlib.figure.Figure(figsize=(6, 5), dpi=80)
     ax = fig.add_subplot(111)
 
     ax.spines["top"].set_visible(False)
